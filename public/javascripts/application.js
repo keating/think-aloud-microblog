11d4d01656a5c690e835c2c9742d52935e3ceab4
Keating <outofclouds@gmail.com>
2012-08-24 08:42:00 +0800

    test

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,16 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-$(function(){
-    $(window).scroll(function (){
-//        $("#header").css("top", $(window).scrollTop());
-//        track_info_site();
-    });
-    function track_info_site() {
-//        var offsetTop = $(window).scrollTop() + ($(window).height()-$("#track_info").height()) / 2;
-//        $("#track_info").animate({top : offsetTop},{duration:500 , queue:false});
+$(function () {
+    var hello = function (status) {
+        return function () {
+            return status;
+        }
+    };
 
-        $("#header").css("top", $(window).scrollTop());
-//        $("#header").animate({top : $(window).scrollTop()},{duration:0 , queue:false});
-    }
-})
+//    var h = hello("wangjiwen")
+//    console.debug(h());
+
+});
